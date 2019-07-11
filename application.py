97c68c887210f428fda338512dfdb9d7550d8f34
Nicholas Hatch <nicholas@thehatchcloud.org>
2019-07-10 22:55:15 -0400

    Mid work on basic SMS directions

--- application.py
+++ application.py
@@ -1,4 +1,8 @@
 from flask import Flask, request
+from twilio.twiml.messaging_response import MessagingResponse
+
+import requests
+import json
 
 app = Flask(__name__)
 
@@ -6,6 +10,31 @@ app = Flask(__name__)
 def index():
     return 'This app returns information over text message.'
 
-@app.route('/directions')
+@app.route("/directions", methods=['POST'])
 def directions():
-    return 'This url will return directions to the user'
+    """Respond to incoming calls with a simple text message."""
+
+    resp = MessagingResponse()
+
+    body = request.values.get('Body', None).lower()
+    if body == 'ask':
+        message = 'To ask for directions, enter: from: <point 1> to: <point 2>' 
+    
+    # Check for from: and to: in the string
+    if 'from:' in body and 'to:' in body:
+        body = body.replace('from: ', 'from:').replace('to: ', 'to:')
+        to_location = body.find('to:')
+        wp0 = body[5:to_location-1]
+        wp1 = body[to_location+3:]
+
+        message = f'wp0 is {wp0}\nwp1 is {wp1}'
+    
+    else:
+        message = "I don't know what you said. Enter 'ask' for help."
+        resp.message(message)
+        return str(resp)
+
+
+
+    resp.message(message)
+    return str(resp)
