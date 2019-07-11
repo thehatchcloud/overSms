307ae42e9a986eb9a958bbbb6232c3a69adbaa03
Nicholas Hatch <nicholas@thehatchcloud.org>
2019-07-10 21:45:32 -0400

    Added a directions route and updated .gitignore.

--- application.py
+++ application.py
@@ -5,3 +5,7 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     return 'This app returns information over text message.'
+
+@app.route('/directions')
+def directions():
+    return 'This url will return directions to the user'
